ac7fec5e55eca44b718fb346a598d479844b0269
madina-madiko <madina98madiko.com@gmail.com>
2018-02-26 01:14:15 +0600

    Create Timer.js

---
+++ timeTracker/Timer.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+
+import helpers from 'C:/Users/Admin/Desktop/WEB/tracking_timer/src/helpers.js';
+
+
+
+class Timer extends React.Component {
+
+
+
+    handleTrashClick = () => {
+
+      this.props.onTrashClick(this.props.id);
+
+    };
+
+
+
+    componentDidMount() {
+
+      this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50);
+
+    }
+
+
+
+    componentWillUnmount() {
+
+      clearInterval(this.forceUpdateInterval);
+
+    }
+
+
+
+    render() {
+
+      const elapsedString = helpers.renderElapsedString(this.props.elapsed, this.props.runningSince);
+
+      return (
+
+        <div className='ui centered card'>
+
+          <div className='content'>
+
+            <div className='header'>{this.props.title}</div>
+
+            <div className='meta'>{this.props.project}</div>
+
+            <div className='center aligned description'>
+
+              <h2>{elapsedString}</h2>
+
+            </div>
+
+            <div className='extra content'>
+
+              <span className='right floated edit icon' onClick={this.props.onEditClick}>
+
+                <i className='edit icon' />
+
+              </span>
+
+              <span className='right floated trash icon' onClick={this.handleTrashClick}>
+
+                <i className='trash icon' />
+
+              </span>
+
+            </div>
+
+          </div>
+
+          <div className='ui bottom attached green basic button'>
+
+            Start
+
+          </div>
+
+        </div>
+
+      );
+
+    }
+
+  }
+
+
+
+  export default Timer;
