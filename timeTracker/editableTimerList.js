028bdb038f7810f0c99e41e627860d62184cce11
madina-madiko <madina98madiko.com@gmail.com>
2018-02-26 01:13:46 +0600

    Create editableTimerList.js

---
+++ timeTracker/editableTimerList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import EditableTimer from './EditableTimer.js';
+
+
+
+class EditableTimerList extends React.Component {
+
+    render() {
+
+      const timers = this.props.timers.map((timer) => 
+
+      (
+
+        <EditableTimer
+
+          key = {timer.id}
+
+          id = {timer.id}
+
+          title = {timer.title}
+
+          project = {timer.project}
+
+          elapsed = {timer.elapsed}
+
+          runningSince = {timer.runningSince}
+
+          onFormSubmit={this.props.onFormSubmit}
+
+          onTrashClick={this.props.onTrashClick}
+
+        />
+
+      )
+
+    );
+
+      return (
+
+        <div id='timers'>
+
+        {timers}
+
+        </div>
+
+      );
+
+    }
+
+  }
+
+
+
+  export default EditableTimerList;
