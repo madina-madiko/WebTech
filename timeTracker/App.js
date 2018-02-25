a5a3396e7df46435e3a0219c3321a3d8464fae08
madina-madiko <madina98madiko.com@gmail.com>
2018-02-26 01:12:22 +0600

    Create App.js

---
+++ timeTracker/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import TimersDashboard from './TimersDashboard.js';
+
+import './App.css';
+
+import './semantic.css';
+
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <TimersDashboard />
+
+    );
+
+  }
+
+}
+
+
+
+export default App;
